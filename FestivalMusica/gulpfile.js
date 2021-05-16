6f477a5107d0d41b4968aa689741d0cdebdf3eb2
Juan David <juandavid918@gmail.com>
2021-05-16 09:08:26 -0500

    Lineup proyecto festival música finalizado

--- FestivalMusica/gulpfile.js
+++ FestivalMusica/gulpfile.js
@@ -1,23 +1,48 @@
 const { series, src, dest, watch } = require("gulp");
 const sass = require("gulp-sass");
+const imagemin = require("gulp-imagemin");
+const notify = require("gulp-notify");
+const webp = require("gulp-webp");
+
+const paths = {
+    imagenes: "src/img/**/*",
+    scss: "src/scss/**/*.scss"
+};
 
 // Función que compila SASS
 function css() {
-    return src("./src/scss/app.scss")
+    return src(paths.scss)
         .pipe(sass({ outputStyle: "expanded" }))
         .pipe(dest("./build/css"));
 }
 
 function minificarcss() {
-    return src("./src/scss/app.scss")
+    return src(paths.scss)
         .pipe(sass({ outputStyle: "compressed" }))
         .pipe(dest("./build/css"));
 }
 
+function imagenes() {
+    return src(paths.imagenes)
+        .pipe(imagemin())
+        .pipe(dest("./build/img"))
+        .pipe(notify({ message: "Imagen Minificada" }));
+}
+
+function versionWebp() {
+    return src(paths.imagenes)
+        .pipe(webp())
+        .pipe(dest("./build/img"))
+        .pipe(notify({ message: "Versión webP lista" }));
+}
+
 function watchArchivos() {
-    watch("./src/scss/**/*.scss", css); // * = La carpeta actual - ** = Todos los archivos con esa extensión
+    watch(paths.scss, css); // * = La carpeta actual - ** = Todos los archivos con esa extensión
 }
 
 exports.css = css;
 exports.minificarcss = minificarcss;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
+
+exports.default = series(css, imagenes, versionWebp, watchArchivos);
