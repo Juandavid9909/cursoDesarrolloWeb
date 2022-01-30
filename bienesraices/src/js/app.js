9300c2f588740a597da9537599e0f16b0c033f14
Juan David <juandavid918@gmail.com>
2022-01-30 12:13:26 -0500

    Dark mode bienes raices finalizado

--- bienesraices/src/js/app.js
+++ bienesraices/src/js/app.js
@@ -1,6 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
     eventListeners();
+
+    darkMode();
 });
+
+function darkMode() {
+    const prefiereDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add("dark-mode");
+    } else {
+        document.body.classList.remove("dark-mode");
+    }
+
+    prefiereDarkMode.addEventListener("change", () => {
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add("dark-mode");
+        } else {
+            document.body.classList.remove("dark-mode");
+        }
+    });
+
+    const botonDarkMode = document.querySelector(".dark-mode-boton");
+
+    botonDarkMode.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+    });
+}
 
 function eventListeners() {
     const mobileMenu = document.querySelector(".mobile-menu");
@@ -10,6 +36,7 @@ function eventListeners() {
 
 function navegacionResponsive() {
     const navegacion = document.querySelector(".navegacion");
+    // JavaScript(ES6)
 
     navegacion.classList.toggle("mostrar");
 }
